6c026398311a829b8f5b341823ec193e61076f42
Pollo <valenv.dg@gmail.com>
2019-04-25 03:51:05 -0600

    Project Alpha Version

---
+++ TestApp2/TestApp2/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestApp2
+{
+    public partial class MainPage : ContentPage
+    {
+
+        const String DEFAULT_USERNAME = "diego2102";
+        const String DEFAULT_PASSWORD = "password";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void SignInButton_Clicked(object sender, EventArgs e)
+        {
+            if ( (UsernameField.Text == DEFAULT_USERNAME) && (PasswordField.Text == DEFAULT_PASSWORD) )
+            {
+                await Navigation.PushModalAsync(new MainScreen());
+
+            }
+            else if ((UsernameField.Text != DEFAULT_USERNAME) && (PasswordField.Text != DEFAULT_PASSWORD))
+            {
+                Console.WriteLine("Incorrect Username and Password.");
+            }
+            else if (UsernameField.Text != DEFAULT_USERNAME)
+            {
+                Console.WriteLine("Incorrect Username");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Password");
+            }
+
+        }
+    }
+}
---
+++ TestApp2/TestApp2/MainScreen.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestApp2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainScreen : ContentPage
+	{
+		public MainScreen ()
+		{
+			InitializeComponent ();
+		}
+
+        private void encryptButton_Clicked (object sender, EventArgs e)
+        {
+            String tempString = EncryptText(textEntry.Text);
+
+            displayLabel.Text = tempString;
+        }
+
+        enum Letters
+        {
+            d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a,b,c
+        }
+
+        public String EncryptText(String targetText)
+        {
+            String encryptedString = null;
+
+            for (int i = 0; i < targetText.Length; i++)
+            {
+                encryptedString += Enum.GetName(typeof(Letters), ((int)targetText[i] % 26));
+            }
+
+            return encryptedString;
+        }
+
+        public String DecryptText(String targetText)
+        {
+            String decryptedText = null;
+
+
+            return decryptedText;
+        }
+	}
+}
